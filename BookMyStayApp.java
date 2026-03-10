ac78a11c21d93a97c4ece5c70fbf91abd33be5b3
sn2047-dot <sn2047@srmist.edu.in>
2026-03-10 10:33:02 +0530

    Create BookMyStayApp.java

---
+++ BookMyStayApp.java
@@ -0,0 +1,6 @@
+public class UseCase1HotelBookingApp{
+public static void main(String[] args){
+System.out.println(&quot;welcome to book my stay&quot;);
+System.out.println(&quot;hotel booking system v1.0&quot;);
+}
+}
